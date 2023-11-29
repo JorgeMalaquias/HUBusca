8898ef786a8b668ffb07af46b631b2b07224d478
jorge <jorgemalaquia@gmail.com>
2023-11-29 20:17:00 -0300

    Initial settings

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import styled from "styled-components/native";
+import {
+  useFonts,
+  Inter_300Light,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_300Light,
+    Inter_900Black,
+  });
+  if (!fontsLoaded) {
+    return;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StyledView>
+      <StyledText>Hello!</StyledText>
+      <StatusBar barStyle="light-content" translucent />
+    </StyledView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const StyledView = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+  align-items: center;
+  justify-content: center;
+`;
+const StyledText = styled.Text`
+  color: #ff0000;
+  font-family: "Inter_300Light";
+`;
